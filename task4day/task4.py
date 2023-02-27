312f48e1cac8acff18c0a9334d6eb717ea8c5a61
KarinaSH7 <karinashalia@yandex.ru>
2023-02-27 18:52:57 +0300

    I did task4

---
+++ task4day/task4.py
@@ -0,0 +1,10 @@
+educator = []
+students = []
+name = input('введите имя:')
+position = input('введите должность:')
+number_of_students = input('введите количество учеников').split()
+students.append(number_of_students)
+educator.append(name)
+educator.append(position)
+educator.append(students)
+print(educator)
