bcd3e6b711d908dad05b396a4a4cc3e52d24e775
KarinaSH7 <karinashalia@yandex.ru>
2023-03-05 16:50:09 +0300

    i did tasks

--- task5day/task5.py
+++ task5day/task5.py
@@ -1,8 +1,6 @@
 # Дан словарь email-адресов студентов, в качестве ключа используется домен, а в качестве значения список имен.
 # Необходимо вывести все email-адреса в формате Имя@домен.
-# addr = map(lambda k: list(map(lambda name: name + '@' + k, emails[k])), emails)
-#
-# print('\n'.join(sorted(sum(addr, []))))
+
 
 emails = {'mgu.edu': ['andrei_serov', 'alexander_pushkin', 'elena_belova', 'kirill_stepanov'],
 		  'gmail.com': ['alena.semyonova', 'ivan.polekhin', 'marina_abrabova'],
