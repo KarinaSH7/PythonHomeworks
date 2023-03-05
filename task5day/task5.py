79c9b32bd53f2b7856a7cf2b1f1bd4f24057adbc
KarinaSH7 <karinashalia@yandex.ru>
2023-03-05 16:48:15 +0300

    code refactoring

--- task5day/task5.py
+++ task5day/task5.py
@@ -5,11 +5,12 @@
 # print('\n'.join(sorted(sum(addr, []))))
 
 emails = {'mgu.edu': ['andrei_serov', 'alexander_pushkin', 'elena_belova', 'kirill_stepanov'],
-      	'gmail.com': ['alena.semyonova', 'ivan.polekhin', 'marina_abrabova'],
-      	'msu.edu': ['sergei.zharkov', 'julia_lyubimova', 'vitaliy.smirnoff'],
-      	'yandex.ru': ['ekaterina_ivanova', 'glebova_nastya'],
-      	'harvard.edu': ['john.doe', 'mark.zuckerberg', 'helen_hunt'],
-      	'mail.ru': ['roman.kolosov', 'ilya_gromov', 'masha.yashkina']}
+		  'gmail.com': ['alena.semyonova', 'ivan.polekhin', 'marina_abrabova'],
+		  'msu.edu': ['sergei.zharkov', 'julia_lyubimova', 'vitaliy.smirnoff'],
+		  'yandex.ru': ['ekaterina_ivanova', 'glebova_nastya'],
+		  'harvard.edu': ['john.doe', 'mark.zuckerberg', 'helen_hunt'],
+		  'mail.ru': ['roman.kolosov', 'ilya_gromov', 'masha.yashkina']}
+
 addr = map(lambda k: list(map(lambda name: name + '@' + k, emails[k])), emails)
 
 print('\n'.join(sum(addr, [])))
