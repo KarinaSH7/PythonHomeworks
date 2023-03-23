f529220e3e24e02fbe0682ac7575422b8b811bca
KarinaSH7 <karinashalia@yandex.ru>
2023-03-23 23:01:11 +0300

    i did tasls

---
+++ task10day/task5.py
@@ -0,0 +1,14 @@
+"""
+Напишите программу-имитатор подбрасывания 2 кубиков.
+Программа выводит на экран "подбрасываю кубики" и спустя 2 секунды выводит значения на кубиках в одну строку.
+"""
+import random
+import time
+
+print('ИГРА В КУБИКИ НАЧАЛАСЬ\n''подбрасываю кубики')
+time.sleep(2)
+random_number1 = random.randint(1, 6)
+random_number2 = random.randint(1, 6)
+print('Вам выпали числа: ', random_number1, random_number2)
+
+
